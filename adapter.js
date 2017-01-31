b773a9554b3980f42b5bad6750ee8b05eddcb5c3
spethso <spethso@studi.informatik.uni-stuttgart.de>
2017-01-31 17:35:19 +0100

    Swagger object is now created for every operation in proto file

--- adapter.js
+++ adapter.js
@@ -12,7 +12,10 @@ var package = protoData.package;
 var operations = protoData.operations;
 // Proto messages
 var messages = protoData.messages;
+// Utility package to inspect json objects
+const util = require('util');
 
+// Standard param
 var stdParam = {
     name: 'start',
     in: 'query',
@@ -20,6 +23,7 @@ var stdParam = {
     default: true
 };
 
+// Instance unique identifier param
 var instanceIDParam = {
     name: 'id',
     in: 'path',
@@ -28,6 +32,7 @@ var instanceIDParam = {
     type: 'string'
 }
 
+// Standard 202 response with instance path
 var response_202 = {
     '202': {
         description: 'Instance resource',
@@ -43,14 +48,7 @@ var response_202 = {
     }
 };
 
-var idParam = {
-    name: 'id',
-    in: 'path',
-    description: 'Unique identifier of instance',
-    required: true,
-    type: 'string'
-};
-
+// Empty body response with 200 code
 var response_200_empty = {
     '200': { description: 'Empty body' }
 };
@@ -69,126 +67,128 @@ var swagger = {
 };
 
 function getPaths() {
-    var operation = operations[0];
-    var pathName = '/' + package + '/' + operation.name;
-    var pathObject = {
-        post: {
-            summary: operation.name,
-            consumes: ['application/json'],
-            parameters: [
-                stdParam,
-                {
-                    name: 'input',
-                    in: 'body',
-                    schema: { $ref: '#/definitions/' + operation.request.name }
-                }
-            ],
-            responses: response_202
-        }
-    };
-    // Add path to paths with specific name and path object
-    swagger.paths[pathName] = pathObject;
-
-    // Operation path with form
-    pathName += '/form';
-    pathObject = {
-        post: {
-            summary: operation.name,
-            consumes: ['multipart/form-data'],
-            parameters: [
-                stdParam,
-                {
-                    name: '', // TODO: PLACEHOLDER_FIELD_NAME
-                    in: 'formData',
-                    type: 'TODO' // TODO derive from proto
-                }
-            ],
-            responses: response_202
-        }
-    };
-    // Add path to paths with specific name and path object
-    swagger.paths[pathName] = pathObject;
-
-    // Operation on instances
-    pathName += '/instances/{id}';
-    var paramArray = [idParam];
-    var res = {
-        '200': 'Instance resource',
-        schema: { $ref: '#/definitions/Instance' }
-    };
-    if (operation.response.isStream == false) {
-        // Add second param if operation does NOT have out stream
-        paramArray.push({
-            name: 'excludeOutput',
-            in: 'query',
-            description: 'Omit instance output in response',
-            type: 'boolean',
-            default: false
-        });
-        // Set response to normal instance resource without stream
-        res = {
-            '200': 'Instance resource',
-            schema: {
-                type: 'object',
-                allOf: [
-                    { $ref: '#/definitions/Instance' },
+    operations.forEach(function (operation) {
+        var pathName = '/' + package + '/' + operation.name;
+        var pathObject = {
+            post: {
+                summary: operation.name,
+                consumes: ['application/json'],
+                parameters: [
+                    stdParam,
                     {
-                        type: 'object',
-                        properties: {
-                            out: { $ref: '#/definitions/' + operation.response.name }
-                        }
+                        name: 'input',
+                        in: 'body',
+                        schema: { $ref: '#/definitions/' + operation.request.name }
                     }
-                ]
+                ],
+                responses: response_202
             }
         };
-    }
-    pathObject = {
-        patch: {
-            summary: 'Update instance resource',
-            consumes: ['application/json'],
-            parameters: [
-                idParam,
-                {
-                    name: 'instance',
-                    in: 'body',
-                    description: 'Updated parts of instance resource',
-                    required: true,
-                    schema: { $ref: '#/definitions/InstanceWritable' }
-                }
-            ],
-            tags: ['Instances'],
-            responses: response_200_empty
-        },
-        get: {
-            summary: 'Get instance resource',
-            produces: ['application/json'],
-            parameters: paramArray
-        },
-        tags: ['Instances'],
-        responses: res
-    };
-    // Add path to paths with specific name and path object
-    swagger.paths[pathName] = pathObject;
+        // Add path to paths with specific name and path object
+        swagger.paths[pathName] = pathObject;
 
-    if (operation.request.isStream == false) {
-        noRequestStreamSwaggerPart(operation, pathName);
-    }
-    if (operation.response.isStream == false) {
-        noResponseStreamSwaggerPart(operation, pathName);
-    }
-    if (operation.request.isStream == true) {
-        requestStreamSwaggerPart(operation, pathName);
-    }
-    if (operation.response.isStream == true) {
-        responseStreamSwaggerPart(operation, pathName);
-    }
-    if (operation.request.isStream == true && operation.response.isStream == true) {
-        bidirectionalStreamSwaggerPart(operation, pathName);
-    }
+        // Operation path with form
+        pathName += '/form';
+        pathObject = {
+            post: {
+                summary: operation.name,
+                consumes: ['multipart/form-data'],
+                parameters: [
+                    stdParam,
+                    {
+                        name: 'TODO', // TODO: PLACEHOLDER_FIELD_NAME
+                        in: 'formData',
+                        type: 'TODO' // TODO derive from proto
+                    }
+                ],
+                responses: response_202
+            }
+        };
+        // Add path to paths with specific name and path object
+        swagger.paths[pathName] = pathObject;
+
+        // Operation on instances
+        pathName += '/instances/{id}';
+        var paramArray = [instanceIDParam];
+        var res = {
+            '200': 'Instance resource',
+            schema: { $ref: '#/definitions/Instance' }
+        };
+        if (operation.response.isStream == false) {
+            // Add second param if operation does NOT have out stream
+            paramArray.push({
+                name: 'excludeOutput',
+                in: 'query',
+                description: 'Omit instance output in response',
+                type: 'boolean',
+                default: false
+            });
+            // Set response to normal instance resource without stream
+            res = {
+                '200': 'Instance resource',
+                schema: {
+                    type: 'object',
+                    allOf: [
+                        { $ref: '#/definitions/Instance' },
+                        {
+                            type: 'object',
+                            properties: {
+                                out: { $ref: '#/definitions/' + operation.response.name }
+                            }
+                        }
+                    ]
+                }
+            };
+        }
+        pathObject = {
+            patch: {
+                summary: 'Update instance resource',
+                consumes: ['application/json'],
+                parameters: [
+                    instanceIDParam,
+                    {
+                        name: 'instance',
+                        in: 'body',
+                        description: 'Updated parts of instance resource',
+                        required: true,
+                        schema: { $ref: '#/definitions/InstanceWritable' }
+                    }
+                ],
+                tags: ['Instances'],
+                responses: response_200_empty
+            },
+            get: {
+                summary: 'Get instance resource',
+                produces: ['application/json'],
+                parameters: paramArray,
+                tags: ['Instances'],
+                responses: res
+            }
+        };
+        // Add path to paths with specific name and path object
+        swagger.paths[pathName] = pathObject;
+
+        if (operation.request.isStream == false) {
+            noRequestStreamSwaggerPart(operation, pathName);
+        }
+        if (operation.response.isStream == false) {
+            noResponseStreamSwaggerPart(operation, pathName);
+        }
+        if (operation.request.isStream == true) {
+            requestStreamSwaggerPart(operation, pathName);
+        }
+        if (operation.response.isStream == true) {
+            responseStreamSwaggerPart(operation, pathName);
+        }
+        if (operation.request.isStream == true && operation.response.isStream == true) {
+            bidirectionalStreamSwaggerPart(operation, pathName);
+        }
+    });
+
 }
 
 /**
- * Function to 
+ * Function to fill swagger definitions part
  */
 function getDefinitions() {
     var definitions = {
@@ -222,12 +222,15 @@ function getDefinitions() {
 
     // Add message objects to definitions part
     messages.forEach(function (message) {
-        m = {
+        var m = {
             type: 'object',
-            properties: {
-                
-            }
+            properties: {}
         };
+        // Add properties to message
+        message.fields.forEach(function (field) {
+            var f = { type: field.type }; // TODO: If type is message than change to object with schema reference
+            m.properties[field.name] = f;
+        });
         // Add message to definitions part
         definitions[message.name] = m;
     });
@@ -390,7 +393,8 @@ function main() {
     // console.log(operations[0].request.name)
     getPaths();
     getDefinitions();
-    console.log(swagger.definitions);
+    console.log(util.inspect(swagger, { depth: 10, colors: true }));
+    //fs.writeFile(__dirname + '/swagger.json', JSON.stringify(swagger));
 }
 
 main();
